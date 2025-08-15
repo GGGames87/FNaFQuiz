abe4dcbe71c326277e4629ed157c9f21918bdfd7
GGGames87 <82972185+GGGames87@users.noreply.github.com>
2025-08-15 04:08:50 +0200

    Update script.js

--- fnafquiz/script.js
+++ fnafquiz/script.js
@@ -18,7 +18,6 @@ let usedSolveAll = false;
 
 const btnSolve = document.getElementById("btn-solve-all");
 const btnSave  = document.getElementById("btn-save");
-// const btnLoad  = document.getElementById("btn-load");
 
 
 const fileInput = document.createElement("input");
